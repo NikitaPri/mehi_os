13c01581dc4492d363740a3c5ced18874f5c8d17
NikitaPri <NikitasPri@users.noreply.github.com>
2016-11-21 17:00:46 +0300

    Удаление пользователя

--- delete_user.sh
+++ delete_user.sh
@@ -1,13 +1,27 @@
-#! /bin/bash
+#!/bin/bash
 delete_user () {
         cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1}' | cat -n
-	echo "Выберите номер пользователя, которогогогогогогогогогогогогогогогогогогогогогого хотите удалить, если хотите удалить по имени, то введите имя пользователя с ключом -u"
+	echo "Выберите номер пользователя, которого хотите удалить, если хотите удалить по имени, то введите имя пользователя с ключом -u"
 	read choice key
 	if [ "$key" == "-u" ]
 	then
 		user=choice
 	else
-		cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1}' | cat -n | awk -FS '{print $1}' | awk -FS '{print $4}'
+		user=$(cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1 }' | cat -n |  awk '($1=="'$choice'"){print $2}')
 	fi
+	read -p "Вы уверены, что хотите удалить пользователя $user" -n 1 -r
+	echo      
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+		exit 1
+	fi
+	userdel $user
+	read -p "Удалить домашний каталог пользователя $user?" -n 1 -r
+	echo    
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+		exit 1
+	fi
+	rm -r /home/$user/ 
 }
 delete_user
