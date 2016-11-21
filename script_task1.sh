5202cb966970e0043b565b6b008c5df7bc174df6
Комиссаров Семён Михайлович <semenkomissarov@mail.ru>
2016-11-22 02:37:47 +0300

    добавил main, проверку на запуск от рута, --help

--- script_task1.sh
+++ script_task1.sh
@@ -7,99 +7,118 @@ breakmenushow () {
 		((i++))
 	done
 }
-PS3='Menu'
+PS3='Your choice: '
 options=("Управление пользователями" "Управление группами" "Поиск пользователей или групп" "Справка" "Выход")
 optionsuser=("Удалить пользователя" "Блокировка пользователя" "Добавить пользователя" "Добавить пользователя в группу" "Смена пароля пользователя" "Справка" "Выход")
 optionsgroup=("Добавить группу" "Удалить группу" "Изменить состав группы" "Справка" "Выход")
 optionsgroupsostav=("Добавить пользователя в группу" "Удалить пользователя из группы" "Справка" "Выход")
 optionssearch=("Найти пользователя" "Найти группу" "Справка" "Выход")
-select opt in "${options[@]}"
-do
-	case $opt in
-		"Управление пользователями")
-			select optuser in "${optionsuser[@]}"
-			do
-				case $optuser in
-					"Удалить пользователя")
-						;;
-					"Блокировка пользователя")
-						;;
-					"Добавить пользователя")
-						;;
-					"Добавить пользователя в группу")
-						;;
-					"Смена пароля пользователя")
-						;;
-					"Справка")
-						;;
-					"Выход")
-						breakmenushow "${options[@]}"
-						break
-						;;
-					*) echo неверный выбор;;
-				esac
-			done
-			;;
-		"Управление группами")
-			select optgroup in "${optionsgroup[@]}"
-			do
-				case $optgroup in
-					"Добавить группу")
-						;;
-					"Удалить группу")
-						;;
-					"Изменить состав группы")
-						select optgroupsostav in "${optionsgroupsostav[@]}"
-						do
-							case $optgroupsostav in
-								"Добавить пользователя в группу")
-									;;
-								"Удалить пользователя из группы")
-									;;
-								"Справка") 
-									;;
-								"Выход")
-									breakmenushow "${optionsgroup[@]}"
-									break
-									;;
-								*) echo неверный выбор;;
-							esac
-						done
-						;;
-					"Справка")
-						;;
-					"Выход")
-						breakmenushow "${options[@]}"
-						break
-						;;
-					*) echo неверный выбор;;
-				esac
-			done
-			;;
-		"Поиск пользователей или групп")
-			select optsearch in "${optionssearch[@]}"
-			do
-				case $optsearch in
-					"Найти пользователя")
-						;;
-					"Найти группу")
-						;;
-					"Справка")
-						;;
-					"Выход")
-						breakmenushow "${options[@]}"
-						break
-						;;
-					*) echo неверный выбор;;
-				esac
-			done
-			;;
-		"Справка")
-			echo "Справка"
-			;;
-		"Выход")
-			break
-			;;
-		*) echo неверный выбор;;
-	esac
-done
+main () {
+	select opt in "${options[@]}"
+	do
+		case $opt in
+			"Управление пользователями")
+				select optuser in "${optionsuser[@]}"
+				do
+					case $optuser in
+						"Удалить пользователя")
+							;;
+						"Блокировка пользователя")
+							;;
+						"Добавить пользователя")
+							;;
+						"Добавить пользователя в группу")
+							;;
+						"Смена пароля пользователя")
+							;;
+						"Справка")
+							;;
+						"Выход")
+							breakmenushow "${options[@]}"
+							break
+							;;
+						*) echo неверный выбор;;
+					esac
+				done
+				;;
+			"Управление группами")
+				select optgroup in "${optionsgroup[@]}"
+				do
+					case $optgroup in
+						"Добавить группу")
+							;;
+						"Удалить группу")
+							;;
+						"Изменить состав группы")
+							select optgroupsostav in "${optionsgroupsostav[@]}"
+							do
+								case $optgroupsostav in
+									"Добавить пользователя в группу")
+										;;
+									"Удалить пользователя из группы")
+										;;
+									"Справка") 
+										;;
+									"Выход")
+										breakmenushow "${optionsgroup[@]}"
+										break
+										;;
+									*) echo неверный выбор;;
+								esac
+							done
+							;;
+						"Справка")
+							;;
+						"Выход")
+							breakmenushow "${options[@]}"
+							break
+							;;
+						*) echo неверный выбор;;
+					esac
+				done
+				;;
+			"Поиск пользователей или групп")
+				select optsearch in "${optionssearch[@]}"
+				do
+					case $optsearch in
+						"Найти пользователя")
+							;;
+						"Найти группу")
+							;;
+						"Справка")
+							;;
+						"Выход")
+							breakmenushow "${options[@]}"
+							break
+							;;
+						*) echo неверный выбор;;
+					esac
+				done
+				;;
+			"Справка")
+				echo "Справка"
+				;;
+			"Выход")
+				break
+				;;
+			*) echo неверный выбор;;
+		esac
+	done
+}
+
+
+if [ "$1" == "--help" ]
+then
+
+	echo "spravka"
+
+else
+
+	if [ $(id -u) != "0" ]
+	then
+        	echo "у вас нет прав на исполнение данного файла" >&2
+        	exit
+	fi
+
+	main "$@"
+fi
