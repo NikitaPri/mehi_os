e8108ed42735a63d2defaf155ee953b470d167e1
Aleksey Avdeev <alexeyavdeev2010@mail.ru>
2016-11-27 13:28:28 +0300

    Изменил поиск группы 1

--- find_group.sh
+++ find_group.sh
@@ -3,7 +3,31 @@
 find_group()
 {
 
+	echo "Введите имя группы, которую хотите найти"
 
+	read group
+
+	is_group=$(cat /etc/group | cut -d : -f1 | awk '($1=="'$group'"){print "true"}')
+
+	if[[ $is_group == "true" ]]
+    then
+
+		echo "Группа найдена"
+		
+		echo $group
+
+	fi	
+
+
+
+    if[[ $is_group!= "true" ]]
+	then
+
+		echo "Группа $group не найдена"
+        
+
+	fi	
+    
 
 }
 find_group
