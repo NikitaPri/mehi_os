c6237cd0910cb4cacc27cc7d46926929da5ed037
Aleksey Avdeev <alexeyavdeev2010@mail.ru>
2016-12-05 14:28:12 +0300

    Добавил Добавление группы1

--- add_group.sh
+++ add_group.sh
@@ -4,5 +4,25 @@ add_group()
 {
 
 
+	echo "Введите имя группы, которую хотите добавить"
+
+	read group
+
+	is_group=$(cat /etc/group | cut -d : -f1 | awk '($1=="'$group'"){print "true"}')
+
+	if [[ $is_group == "true" ]]
+	then
+
+		echo "Группа с таким названием уже существует"
+		add_group
+
+	fi
+	
+	
+	groupadd $group
+
+
+
+
 }
 add_group
