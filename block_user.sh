1883946f2480ab4ecb7ddf4e08d2bfe956633948
Комиссаров Семён Михайлович <semenkomissarov@gmail.com>
2016-12-08 17:41:44 +0300

    блокировка пользователя отредактирована

--- block_user.sh
+++ block_user.sh
@@ -1,24 +1,43 @@
 block_user() {
+	echo
+	echo Блокировка пользователя:
+	echo
+	echo Список пользователей:
 	cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1}' | cat -n
-        echo "Выберите номер пользователя, которого хотите заблокировать, если хотите заблокировать по имени, то введите имя пользователя с ключом -u"
+	echo
+        echo "Ввведите номер пользователя, которого хотите заблокировать или exit чтобы выйти.  Если хотите заблокировать по имени, то введите имя пользователя с ключом -u :"
         read choice key
+
+	if [ "$choice" == "exit" ]
+	then
+		return 0
+	fi
+
         if [ "$key" == "-u" ]
         then
-                user=choice
+                user=$(cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1 }' | awk '($1=="'$choice'")')
         else
                 user=$(cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1 }' | cat -n |  awk '($1=="'$choice'"){print $2}')
         fi
-        locked=$(cat /etc/shadow | grep "\!\!" | cut -d : -f1 | awk '($1=="'$user'"){print "true"}')
-	if [[ "$locked" == "true" ]]
+
+	if [ "$user" == "" ]
 	then
-		read -p "Пользователь $user уже заблокирован, хотите его разблокировать?" -n 1 -r    
-        	if [[ ! $REPLY =~ ^[Yy]$ ]]
+		echo
+		echo Ошибка! Пользователь не найден >&2
+	else
+		echo $user
+        	locked=$(cat /etc/shadow | grep "\!\!" | cut -d : -f1 | awk '($1=="'$user'"){print "true"}')
+		if [[ "$locked" == "true" ]]
 		then
-                	exit 1
-        	fi
-		passwd -u $user
-		exit 1
+			read -p "Пользователь $user уже заблокирован, хотите его разблокировать?" -n 1 -r    
+        		if [[ ! $REPLY =~ ^[Yy]$ ]]
+			then
+                		exit 1
+        		fi
+			passwd -u $user
+			exit 1
+		fi
+		passwd -l $user
 	fi
-	passwd -l $user
 }
 block_user
