1152bd0a1711491a9dafde27d4b742735ff33a17
Комиссаров Семён Михайлович <semenkomissarov@gmail.com>
2016-12-08 19:52:06 +0300

    добавление пользователя в группу и смена пароля отредактированы

--- change_passwd.sh
+++ change_passwd.sh
@@ -1,14 +1,36 @@
 change_passwd() {
-	cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1}' | cat -n
-        echo "Выберите номер пользователя, для которого хотите сменить пароль, если хотите сменить пароль по имени пользователя, то введите имя пользователя с ключом -u"
-        read choice key
-        if [ "$key" == "-u" ]
-        then
-                user=choice
-        else
-                user=$(cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1 }' | cat -n |  awk '($1=="'$choice'"){print $2}')
-        fi
-	echo Введите новый пароль
-	passwd $user
+	while true;
+	do
+		echo
+		echo Смена пароля
+		echo
+		echo Список пользователей:
+		cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1}' | cat -n
+			echo "Введите номер пользователя, для которого хотите сменить пароль или exit чтобы выйти. Если хотите сменить пароль по имени пользователя, то введите имя пользователя с ключом -u"
+		read choice key
+		if [ "$choice" == "exit"  ]
+		then
+			return 0
+		fi
+		if [ "$key" == "-u" ]
+		then
+			user=$(cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1 }' | awk '($1=="'$choice'")')
+		else
+			user=$(cat /etc/passwd | awk -F : '(($3>=1000)&&($1!="nfsnobody")){print $1 }' | cat -n |  awk '($1=="'$choice'"){print $2}')
+		fi
+		if [ "$user" == ""  ]
+		then
+			echo Ошибка! пользователь не найден >&2
+		else
+			echo Введите новый пароль
+			passwd $user
+		fi
+		read -p "Повторить? (Y): " -n 1 -r
+                echo    
+                if [[ ! $REPLY =~ ^[Yy]$ ]]
+                then
+			break
+                fi
+	done
 }
 change_passwd
