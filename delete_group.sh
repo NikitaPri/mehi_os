93a8d2f21b56ac1ffa30bc7ed41797b0292031e7
Aleksey Avdeev <alexeyavdeev2010@mail.ru>
2016-12-10 17:13:19 +0300

    Удаление группы

--- delete_group.sh
+++ delete_group.sh
@@ -2,11 +2,37 @@
 
 delete_group()
 {
+        echo Удаление группы
+
+        while true;
+        do
+
+	       echo "Введите имя группы, которую хотите удалить, или "exit" чтобы выйти"
+
+	       read group
+
+               if [ "$group" == "exit" ]
+               then
+            
+                    return 0;
+               
+               fi 
 
 
-	echo "Введите имя группы, которую хотите удалить"
+               if [ "$(echo $group | grep -x -E "[a-z_][a-z0-9_-]*[$]?")" == "" ]
+	       then
 
-	read group
+		    echo Ошибка! Имя группы должно начинаться со строчной буквы или символа подчёркивания, и должны состоять только из строчных букв, цифр, символов подчёркивания и минусов. Они могут заканчиваться знаком доллара. 
+	       
+               else
+
+		    break
+
+	       fi
+
+        done
+
+	
 
 	is_group=$(cat /etc/group | cut -d : -f1 | awk '($1=="'$group'"){print "true"}')
 
@@ -31,10 +57,10 @@ delete_group()
 	fi
 	
 	
-        echo "Для удаления группы введите y (иначе произойдет выход)"
-        read noexit
+        echo "Повторить? y"
+        read repeat
 
-        if [[ $noexit == "y" ]]
+        if [[ $repeat =~ ^[Yy]$ ]]
         then
 
 		delete_group
