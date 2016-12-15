1eb493947575c64581c04fbf9f7b5104b7ad7ca2
Aleksey Avdeev <alexeyavdeev2010@mail.ru>
2016-12-15 17:17:10 +0300

    Добавление группы

--- add_group.sh
+++ add_group.sh
@@ -3,10 +3,37 @@
 add_group()
 {
 
+        echo Добавление группы
 
-	echo "Введите имя группы, которую хотите добавить"
+        while true;
+        do
 
-	read group
+	       echo "Введите имя группы, которую хотите добавить, или "exit" чтобы выйти"
+
+	       read group
+
+               if [ "$group" == "exit" ]
+               then
+            
+                    return 0;
+               
+               fi 
+
+
+               if [ "$(echo $group | grep -x -E "[a-z_][a-z0-9_-]*[$]?")" == "" ]
+	       then
+
+		    echo Ошибка! Имя группы должно начинаться со строчной буквы или символа подчёркивания, и должны состоять только из строчных букв, цифр, символов подчёркивания и минусов. Они могут заканчиваться знаком доллара. 
+	       
+               else
+
+		    break
+
+	       fi
+
+        done
+
+	
 
 	is_group=$(cat /etc/group | cut -d : -f1 | awk '($1=="'$group'"){print "true"}')
 
@@ -31,10 +58,10 @@ add_group()
 	fi
 	
 	
-        echo "Для добавления группы введите y (иначе произойдет выход)"
-        read noexit
+        echo "Повторить? y"
+        read repeat
 
-        if [[ $noexit == "y" ]]
+        if [[ $repeat =~ ^[Yy]$ ]]
         then
 
 		add_group
