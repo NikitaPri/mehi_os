730facb21652757af72595d60a42f57b32dc04d8
Aleksey Avdeev <alexeyavdeev2010@mail.ru>
2016-12-15 18:52:06 +0300

    Поиск группы

--- find_group.sh
+++ find_group.sh
@@ -3,30 +3,74 @@
 find_group()
 {
 
-	echo "Введите имя группы, которую хотите найти"
+        echo Поиск группы
 
-	read group
+        while true;
+        do
 
-	is_group=$(cat /etc/group | cut -d : -f1 | awk '($1=="'$group'"){print "true"}')
+	       echo "Введите имя группы, которую хотите найти, или "exit" чтобы выйти"
+
+	       read group
+
+               if [ "$group" == "exit" ]
+               then
+            
+                    return 0;
+               
+               fi 
+
+
+               if [ "$(echo $group | grep -x -E "[a-z_][a-z0-9_-]*[$]?")" == "" ]
+	       then
+
+		    echo Ошибка! Имя группы должно начинаться со строчной буквы или символа подчёркивания, и должны состоять только из строчных букв, цифр, символов подчёркивания и минусов. Они могут заканчиваться знаком доллара. 
+	       
+               else
+
+		    break
+
+	       fi
+
+        done
+
+        is_group="true" 
+
+        if [ "$(cat /etc/group | grep -E ^$group )" == "" ]
+        then
+
+                is_group="false"
+        
+        fi
+
 
 	if [[ $is_group == "true" ]]
-    then
+        then
 
 		echo "Группа найдена"
-		
-		echo $group
 
+                echo "$(cat /etc/group | grep -E ^$group | cut -d: -f1,3,5 | tr ":" " " |  awk '{print "1)имя: "$1" 2)gid: "$2" 3)список пользователей, входящих в эту группу:  "$3" "}')"
+		
+		
 	fi	
 
 
 
-    if [[ $is_group != "true" ]]
+        if [[ $is_group == "false" ]]
 	then
 
 		echo "Группа $group не найдена"
         
 
 	fi	
+
+        echo "Повторить? y"
+        read repeat
+
+        if [[ $repeat =~ ^[Yy]$ ]]
+        then
+
+		find_group
+	fi
     
 
 }
