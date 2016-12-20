6a328f332b93dc6e9d4e9ebdf29dda521ac6203f
MarkMuffin <mark@localhost.localdomain>
2016-12-20 12:48:53 +0300

    ,gubgb,tloht/y7yj;.

--- delete_user_group.sh
+++ delete_user_group.sh
@@ -6,10 +6,13 @@ delete_user_group.sh()
 	#cat /etc/group | cut -d : -f3
 	if [ "$key" == "-g" ]
 	then
-		 group=$(cat /etc/group | awk -F : '({print $1 }' | awk '($1=="'$choice'")')
+		 group=$key
 	 else
 		 group=$(cat /etc/group | awk -F : '{print $1 }' | cat -n |  awk '($1=="'$choice'"){print $2}')
 	fi
-	echo $group
+	echo "состав группы"
+	cat /etc/group | awk -F : '($1=="'$group'")' | cut -d : -f4
+	delgroup $user $group
+	
 }
 delete_user_group.sh
